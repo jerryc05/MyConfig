a73ae9f23e7f8dac525f0c93080b1b83c12603bb
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-12-31 04:05:52 -0500

    Update util_before_request.py

--- python/quart/util_before_request.py
+++ python/quart/util_before_request.py
@@ -1,10 +1,9 @@
 from quart import Response, redirect, request
+from http import HTTPStatus
 
 
 def upgrade_http() -> 'Response|None':
     if not request.is_secure:
         url = request.url.replace('http://', 'https://', 1)
-        from http import HTTPStatus
-
         code = HTTPStatus.MOVED_PERMANENTLY
         return redirect(location=url, code=code)
