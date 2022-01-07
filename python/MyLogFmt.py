f39bd738822fbeffe6d23dd090f81f9fedea12f7
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-01-07 02:22:38 -0500

    log_fmt add ms

--- python/MyLogFmt.py
+++ python/MyLogFmt.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from colorama import Fore, Style, init
 
@@ -7,10 +8,10 @@ init()
 
 class MyLogFmt(logging.Formatter):
     FMT_STR = (
-        "[%(asctime)s] [%(levelname)8s] [%(process)d %(processName)s %(filename)s:%(lineno)d]"
-        " %(message)s"
+        "[%(asctime)s] [%(levelname)8s] [%(process)d %(processName)s] [%(thread)d"
+        " %(threadName)s] [%(filename)s:%(lineno)d] %(message)s"
     )
-    DATE_FMT = '%m-%d-%YT%H:%M:%S%z'
+    DATE_FMT = r'%Y-%m-%d_%H:%M:%S.%f%z'
     DFT_FMT = f'{Style.RESET_ALL}{FMT_STR}'
     FORMATS = {
         logging.DEBUG: f'{Fore.CYAN}{FMT_STR}{Style.RESET_ALL}',
@@ -19,6 +20,16 @@ class MyLogFmt(logging.Formatter):
         logging.ERROR: f'{Fore.RED}{FMT_STR}{Style.RESET_ALL}',
         logging.CRITICAL: f'{Fore.RED}{Style.BRIGHT}{FMT_STR}{Style.RESET_ALL}',
     }
+
+    def formatTime(self, record: 'logging.LogRecord', datefmt: 'str|None' = None):
+        dt = datetime.fromtimestamp(record.created).astimezone()
+        if datefmt:
+            s = dt.strftime(datefmt)
+        else:
+            s = dt.strftime(self.default_time_format)
+            if self.default_msec_format:
+                s = self.default_msec_format % (s, record.msecs)
+        return s
 
     def format(self, record: 'logging.LogRecord'):
         self.__init__(
