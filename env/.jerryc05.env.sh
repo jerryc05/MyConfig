4bd80c01a00480ed062794b89975825012027874
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-01-17 23:20:55 -0500

    add llvm and ssh-agent
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

--- env/.jerryc05.env.sh
+++ env/.jerryc05.env.sh
@@ -11,3 +11,34 @@ if [ "$(uname -s)" = "Darwin" ] && [ -d "$(brew --prefix llvm)" ]; then
       __lib="-L$(brew --prefix llvm)/lib"     && { [[ ! "$LFDLAGS"  == *"$__lib"*     ]] && export  LDFLAGS="$LDFLAGS $__lib";      unset __lib }
   __include="-I$(brew --prefix llvm)/include" && { [[ ! "$CPPFLAGS" == *"$__include"* ]] && export CPPFLAGS="$CPPFLAGS $__include"; unset __include }
 fi
+
+# VSCode's LLVM tools
+USER_DIRS=$'/root/\n'
+for d in "/c/Users/" "/mnt/c/Users/" "/home/"; do
+  USER_DIRS="$USER_DIRS`find $d -maxdepth 1 -type d 2>/dev/null`"
+done
+for USER in "$USER_DIRS"; do
+  EXT="$USER/.vscode-insiders/extensions/"
+  [ -d $EXT ] || continue
+  DIR="`find $EXT -maxdepth 1 -type d -name "ms-vscode.cpptools-*"`/LLVM/bin"
+  [ -d $DIR ] && export PATH="$PATH:$DIR"
+done
+
+# SSH Agent
+ssh-add -l >/dev/null 2>&1
+if [ $? -eq 2 ]; then
+  export DOT_SSH="$HOME/.ssh"
+  mkdir -p $DOT_SSH
+
+  export SSH_AUTH_SOCK=$(cat $DOT_SSH/.SSH_AUTH_SOCK 2>/dev/null)
+  export SSH_AGENT_PID=$(cat $DOT_SSH/.SSH_AGENT_PID 2>/dev/null)
+
+  ssh-add -l >/dev/null 2>&1
+  if [ $? -eq 2 ]; then
+    eval `ssh-agent`
+    if [ $? -ne 0 ]; then
+      echo $SSH_AUTH_SOCK >$DOT_SSH/.SSH_AUTH_SOCK
+      echo $SSH_AGENT_PID >$DOT_SSH/.SSH_AGENT_PID
+    fi
+  fi
+fi
