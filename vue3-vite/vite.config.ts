2b7977397baf983decefff837e4d2f48a253944d
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-01-25 02:35:07 -0500

    add minify html plugin
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

--- vue3-vite/vite.config.ts
+++ vue3-vite/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { minifyHtml } from 'vite-plugin-html'
 import path = require("path")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), minifyHtml()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
