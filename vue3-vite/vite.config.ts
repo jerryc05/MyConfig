ead5072f748329f64e9905cd78961157a843449a
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-01-26 01:49:50 -0500

    add MyPostProcessorOnBuild
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

--- vue3-vite/vite.config.ts
+++ vue3-vite/vite.config.ts
@@ -4,10 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import { minifyHtml, injectHtml } from 'vite-plugin-html'
 
+
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    MyPostProcessorOnBuild(p => { }),
     // minifyHtml(), injectHtml({ data: { injectHead: '' } })
   ],
   resolve: {
@@ -33,3 +38,32 @@ export default defineConfig({
     }
   }
 })
+
+
+
+
+
+import { readdir, stat } from 'fs/promises'
+import { Plugin, normalizePath } from 'vite'
+function MyPostProcessorOnBuild(fn: (p:string) => Promise<void> | void): Plugin {
+  let outRoot: string
+  async function iterDir(dir: string) {
+    await readdir(dir).then(xs => xs.forEach(async x => {
+      x = path.resolve(dir, x)
+      await stat(x).then(async s => {
+        if (!s.isDirectory()) await fn(x)
+        else await iterDir(x)
+      })
+    }
+    ))
+  }
+  return {
+    name: MyPostProcessorOnBuild.name,
+    enforce: "post",
+    apply: "build",
+    configResolved(conf) {
+      outRoot = normalizePath(path.resolve(conf.root, conf.build.outDir))
+    },
+    async closeBundle() { await iterDir(outRoot) }
+  }
+}
