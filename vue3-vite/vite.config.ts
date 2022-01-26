80b58584125edbf9fffe657e8c5f08bdef4fec9d
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-01-25 22:34:56 -0500

    style fix
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

--- vue3-vite/vite.config.ts
+++ vue3-vite/vite.config.ts
@@ -1,7 +1,8 @@
+import path = require("path")
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { minifyHtml } from 'vite-plugin-html'
-import path = require("path")
 
 // https://vitejs.dev/config/
 export default defineConfig({
