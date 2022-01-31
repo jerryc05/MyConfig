e5d776f4570f0153b90b4a5f2fd289cb9d8a1ce5
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-01-31 00:38:56 -0500

    make pinia sass optional
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

--- vue3-vite/src/main.ts
+++ vue3-vite/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 createApp(App)
-  .use(createPinia())
+  // .use(createPinia())
   .mount('#app')
