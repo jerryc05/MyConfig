44489137d0ce2f52fb989d25e2549e1794863b81
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-02-08 18:34:44 -0500

    add sqlite
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

---
+++ sql/sqlite/schema.sql
@@ -0,0 +1,7 @@
+pragma synchronous = off;      -- off | normal | full
+pragma journal_mode = memory;  -- off | memory | persist | truncate | wal | delete
+pragma temp_store = memory;
+pragma foreign_keys = on;
+
+.headers on
+.mode column
