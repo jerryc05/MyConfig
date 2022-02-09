e5e529bb776e9df02f6c8a2e75a60daa1db6f8c9
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-02-08 20:47:03 -0500

    sqlite3
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

--- sql/sqlite/schema.sql
+++ sql/sqlite/schema.sql
@@ -2,6 +2,7 @@ pragma synchronous = off;      -- off | normal | full
 pragma journal_mode = memory;  -- off | memory | persist | truncate | wal | delete
 pragma temp_store = memory;
 pragma foreign_keys = on;
+-- sqlite3 db.sqlite3 "vacuum"
 
 .headers on
 .mode column
