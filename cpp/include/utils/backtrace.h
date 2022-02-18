dda4444d31429c38f6a3c54f22477b66cc1bbdde
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-02-18 15:46:27 -0500

    hide backtrace
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

--- cpp/include/utils/backtrace.h
+++ cpp/include/utils/backtrace.h
@@ -5,31 +5,31 @@
 
 #pragma once
 
-#include <execinfo.h>
+// #include <execinfo.h>
 
-#include <cstring>
-#include <iostream>
+// #include <cstring>
+// #include <iostream>
 
-#include "types.h"
+// #include "types.h"
 
 
-template <Usize NUM_TRACES = 16>
-void
-stackTraceSigHandler(int sig) {
-  std::cerr << "\n\033[1;91m"
-               "===== Error: Signal ["
-            << strsignal(sig) << " (" << sig
-            << ")] ====="
-               "\033[0;31m\n";
+// template <Usize NUM_TRACES = 16>
+// void
+// stackTraceSigHandler(int sig) {
+//   std::cerr << "\n\033[1;91m"
+//                "===== Error: Signal ["
+//             << strsignal(sig) << " (" << sig
+//             << ")] ====="
+//                "\033[0;31m\n";
 
-#ifndef NDEBUG
-  Arr<void*, NUM_TRACES> btArr;
-  auto                   traceSize = backtrace(btArr.data(), btArr.size());
+// #ifndef NDEBUG
+//   Arr<void*, NUM_TRACES> btArr;
+//   auto                   traceSize = backtrace(btArr.data(), btArr.size());
 
-  // print out all the frames to stderr
-  backtrace_symbols_fd(btArr.data(), traceSize, stderr);
-#endif
+//   // print out all the frames to stderr
+//   backtrace_symbols_fd(btArr.data(), traceSize, stderr);
+// #endif
 
-  std::cerr << "\033[0m";
-  std::_Exit(128 + sig);
-}
+//   std::cerr << "\033[0m";
+//   std::_Exit(128 + sig);
+// }
