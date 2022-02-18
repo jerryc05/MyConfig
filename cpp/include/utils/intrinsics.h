9dbb960241355427a842f0ce89d05904b8438623
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-02-18 11:31:21 -0500

    add intrinsics
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

---
+++ cpp/include/utils/intrinsics.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019-2022 Ziyan "Jerry" Chen (@jerryc05).
+ *                         All rights reserved.
+ */
+
+#pragma once
+
+#if not _MSC_VER
+#  include <x86intrin.h>
+#else
+#  include <intrin.h>
+#endif
