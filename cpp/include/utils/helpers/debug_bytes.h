0a72b9c04050caa139d687c2e46c18d71fc0f637
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-02-18 19:46:22 -0500

    rm debug bytes
    Signed-off-by: Ziyan "Jerry" Chen <jerryc443@gmail.com>

--- cpp/include/utils/helpers/debug_bytes.h
+++ cpp/include/utils/helpers/debug_bytes.h
@@ -5,48 +5,48 @@
 
 #pragma once
 
-#include <iomanip>
-#include <ostream>
+// #include <iomanip>
+// #include <ostream>
 
-#include "../attributes.h"
-#include "../macros.h"
-#include "../types.h"
+// #include "../attributes.h"
+// #include "../macros.h"
+// #include "../types.h"
 
 
-template <std::size_t MAX_SIZE = 128,
-          class CharT          = char,
-          std::basic_ostream<CharT, std::char_traits<CharT>> OS>
-void
-debugBytes(void* ptr, std::size_t size) {
-  auto charTPtr = reinterpret_cast<CharT*>(ptr);
+// template <std::size_t MAX_SIZE = 128,
+//           class CharT          = char,
+//           std::basic_ostream<CharT, std::char_traits<CharT>> OS>
+// void
+// debugBytes(void* ptr, std::size_t size) {
+//   auto charTPtr = reinterpret_cast<CharT*>(ptr);
 
-  ConstExpr auto hexWidth        = 2 * sizeof(CharT);
-  const auto     sizeToPrint     = std::min(size, MAX_SIZE);
-  const auto     printCharMargin = [sizeToPrint]() {
-    OS << '|' << std::setfill('-')
-       << std::setw(
-                  static_cast<jerryc05::FnTypes<decltype(std::setw)>::ArgT<0>>(hexWidth * sizeToPrint))
-       << "|\n";
-    OS.copyfmt(std::ios(nullptr));
-  };
+//   ConstExpr auto hexWidth        = 2 * sizeof(CharT);
+//   const auto     sizeToPrint     = std::min(size, MAX_SIZE);
+//   const auto     printCharMargin = [sizeToPrint]() {
+//     OS << '|' << std::setfill('-')
+//        << std::setw(
+//                   static_cast<jerryc05::FnTypes<decltype(std::setw)>::ArgT<0>>(hexWidth * sizeToPrint))
+//        << "|\n";
+//     OS.copyfmt(std::ios(nullptr));
+//   };
 
-  OS << "\033[1;94m";
-  printCharMargin();
+//   OS << "\033[1;94m";
+//   printCharMargin();
 
-  for (auto j = 0; j < 2; ++j) {
-    OS << "\033[1;94m|\033[0m";
-    for (std::remove_const_t<decltype(sizeToPrint)> i = 0; i < sizeToPrint; ++i) {
-      if (j == 0) {
-        OS << std::setw(hexWidth) << charTPtr[i];
-      } else {
-        OS << "\033[0;9" << (i & 1 ? '5' : '6') << 'm' << std::setw(hexWidth) << std::hex
-           << +charTPtr[i] << std::dec;
-      }
-    }
-    OS << "\033[1;94m|\033[0m\n" << std::setfill('0');
-  }
+//   for (auto j = 0; j < 2; ++j) {
+//     OS << "\033[1;94m|\033[0m";
+//     for (std::remove_const_t<decltype(sizeToPrint)> i = 0; i < sizeToPrint; ++i) {
+//       if (j == 0) {
+//         OS << std::setw(hexWidth) << charTPtr[i];
+//       } else {
+//         OS << "\033[0;9" << (i & 1 ? '5' : '6') << 'm' << std::setw(hexWidth) << std::hex
+//            << +charTPtr[i] << std::dec;
+//       }
+//     }
+//     OS << "\033[1;94m|\033[0m\n" << std::setfill('0');
+//   }
 
-  OS << "\033[1;94m";
-  printCharMargin();
-  std::cerr << "\033[0m";
-}
+//   OS << "\033[1;94m";
+//   printCharMargin();
+//   std::cerr << "\033[0m";
+// }
