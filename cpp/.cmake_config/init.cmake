01dab660b77446a983008cd99d55f49afe4fa790
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-06 03:54:42 -0500

    Update init.cmake

--- cpp/.cmake_config/init.cmake
+++ cpp/.cmake_config/init.cmake
@@ -7,6 +7,11 @@ if(PROJECT_BINARY_DIR STREQUAL PROJECT_SOURCE_DIR)
 endif()
 
 if(CMAKE_BUILD_TYPE STREQUAL "")
-    message(STATUS "[CMAKE_BUILD_TYPE] not set, using default: Debug")
+    message(STATUS "[CMAKE_BUILD_TYPE] not set, using default: [Debug]")
     set(CMAKE_BUILD_TYPE "Debug")
+endif()
+
+if(CMAKE_POSITION_INDEPENDENT_CODE STREQUAL "")
+    message(STATUS "[CMAKE_POSITION_INDEPENDENT_CODE] not set, using default: ON")
+    set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 endif()
