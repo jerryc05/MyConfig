5c6729984a0493763fadafde7fa9d7e3bd864ed2
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-06 02:39:17 -0500

    upd cmake

--- cpp/.cmake_config/init.cmake
+++ cpp/.cmake_config/init.cmake
@@ -1,6 +1,11 @@
 # Copyright (c) 2019-2022 Ziyan "Jerry" Chen (@jerryc05).
 #                         All rights reserved.
 
+
+if(PROJECT_BINARY_DIR STREQUAL PROJECT_SOURCE_DIR)
+    message(FATAL_ERROR "[BINARY_DIR] must not be the same as [SOURCE_DIR]!")
+endif()
+
 if(CMAKE_BUILD_TYPE STREQUAL "")
     message(STATUS "[CMAKE_BUILD_TYPE] not set, using default: Debug")
     set(CMAKE_BUILD_TYPE "Debug")
--- cpp/.cmake_config/symlink-compile-commands.cmake
+++ cpp/.cmake_config/symlink-compile-commands.cmake
@@ -4,9 +4,5 @@
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-if(CMAKE_BINARY_DIR STREQUAL PROJECT_SOURCE_DIR)
-    message(FATAL_ERROR "[BINARY_DIR] = [SOURCE_DIR]! Make sure you are running cmake from the build directory!")
-endif()
-
-file(CREATE_LINK ${CMAKE_BINARY_DIR}/compile_commands.json ${PROJECT_SOURCE_DIR}/compile_commands.json
+file(CREATE_LINK ${PROJECT_BINARY_DIR}/compile_commands.json ${PROJECT_SOURCE_DIR}/compile_commands.json
      COPY_ON_ERROR SYMBOLIC)
