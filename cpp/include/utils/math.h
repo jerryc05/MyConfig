640d9d6f256892f9374c90352f5e060c107154ba
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-07 18:02:22 -0500

    upd math

--- cpp/include/utils/math.h
+++ cpp/include/utils/math.h
@@ -6,25 +6,35 @@
 #pragma once
 
 #include <cmath>
+#include <type_traits>
 
 #include "attributes.h"
 
 namespace jerryc05 {
 
-  constexpr ForceInline auto _clz(unsigned int x) { return __builtin_clz(x); }
+  template <class T, std::enable_if_t<std::is_same_v<T, unsigned int>, bool> = true>
+  constexpr ForceInline auto _clz(T x) {
+    return __builtin_clz(x);
+  }
 
-  constexpr ForceInline auto _clz(unsigned long x) { return __builtin_clzl(x); }
+  template <class T, std::enable_if_t<std::is_same_v<T, unsigned long>, bool> = true>
+  constexpr ForceInline auto _clz(T x) {
+    return __builtin_clzl(x);
+  }
 
-  constexpr ForceInline auto _clz(unsigned long long x) { return __builtin_clzll(x); }
+  template <class T, std::enable_if_t<std::is_same_v<T, unsigned long long>, bool> = true>
+  constexpr ForceInline auto _clz(T x) {
+    return __builtin_clzll(x);
+  }
 
   template <class T>
   constexpr ForceInline auto log2_floor(T x) {
-    constexpr auto clz = _clz(x);
+    const auto& clz = _clz(x);
     return static_cast<decltype(clz)>(std::numeric_limits<decltype(x)>::digits - 1) - clz;
   }
 
   template <class T>
-  constexpr ForceInline T log2_ceil(T x) {
+  constexpr ForceInline auto log2_ceil(T x) {
     return log2_floor(x - 1) + 1;
   }
 }  // namespace jerryc05
