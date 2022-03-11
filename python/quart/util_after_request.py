fd0e2e9a0e1738d17adfa3285a3b344d13acef23
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-11 17:57:41 -0500

    add csp

--- python/quart/util_after_request.py
+++ python/quart/util_after_request.py
@@ -1,13 +1,14 @@
 import gzip
 from typing import cast
 
-from quart import Response, request
+from quart import Request, Response
+from werkzeug.datastructures import ContentSecurityPolicy
 
 
-async def compress(res: Response) -> Response:
+async def compress(res: Response, req: Request) -> Response:
     GZIP = 'gzip'
     IDENTITY = 'identity'
-    if GZIP in request.accept_encodings and (
+    if GZIP in req.accept_encodings and (
         not res.content_encoding or res.content_encoding == IDENTITY
     ):
         data = res.get_data(as_text=False)
@@ -26,4 +27,10 @@ def remove_headers(res: Response) -> Response:
     del res.headers['Expires']
     del res.headers['Last-Modified']
     del res.headers['Server']
+    return res
+
+
+def add_csp(res: Response) -> Response:
+    res.content_security_policy = ContentSecurityPolicy()
+    res.content_security_policy.default_src = "'self'"
     return res
