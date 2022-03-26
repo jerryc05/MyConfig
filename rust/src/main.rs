1c753499f9112d0b456cccf8e302432900f6252b
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-25 23:52:53 -0400

    upd rust now ok

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,18 @@
+#![no_main]
+
 #[macro_use]
 pub mod utils;
 
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Write;
+use std::os::unix::io::FromRawFd;
+
+fn stdout() -> File {
+    unsafe { File::from_raw_fd(1) }
+}
+
+#[no_mangle]
+pub fn main(_argc: i32, _argv: *const *const u8) {
+    let mut stdout = stdout();
+    stdout.write(b"Hello, world!\n").unwrap();
 }
---
+++ rust/src/main_.rs
@@ -0,0 +1,7 @@
+#[macro_use]
+pub mod utils;
+
+fn main() {
+    println!("Hello, world!");
+    panic!("This is a {}!", "panic");
+}
