40e095c87e68cb9947ae51db266203804a67f579
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-26 16:58:16 -0400

    add more ncurses

--- rust/parallel-test/src/main.rs
+++ rust/parallel-test/src/main.rs
@@ -10,23 +10,22 @@ use crate::utils::*;
 
 #[no_mangle]
 pub fn main(_argc: i32, _argv: *const *const u8) -> i32 {
-    let w: WINDOW = initscr();
+    let w = NcursesWindow::default();
     {
-        let x = getcurx(w);
-        let y = getcury(w);
-        addstr(&format!("{} {}", x, y));
-        mv(y + 1, 0);
+        let x = w.getcurx();
+        let y = w.getcury();
+        w.addstr(&format!("{} {}", x, y));
+        w.move_(y + 1, 0);
     }
-    addstr("Hello, world! Ncurses!");
+    w.addstr("Hello, world! Ncurses!");
     {
-        let x = getcurx(w);
-        let y = getcury(w);
-        addstr(&format!("{} {}", x, y));
-        mv(y + 1, 0);
+        let x = w.getcurx();
+        let y = w.getcury();
+        w.addstr(&format!("{} {}", x, y));
+        w.move_(y + 1, 0);
     }
-    refresh();
-    getch();
-    endwin();
+    w.refresh();
+    w.getch();
 
     0
 }
--- rust/src/utils/ncurses.rs
+++ rust/src/utils/ncurses.rs
@@ -1,10 +1,52 @@
-pub use ncurses::*;
+use ncurses::*;
+
+pub use ncurses::{KEY_F0, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F9};
+pub use ncurses::{KEY_F10, KEY_F11, KEY_F12, KEY_F13, KEY_F14, KEY_F15};
 
 #[derive(Debug)]
-struct NcursesWindow(WINDOW);
+pub struct NcursesWindow(WINDOW);
+
+impl Default for NcursesWindow {
+    fn default() -> Self {
+        NcursesWindow(initscr())
+    }
+}
 
 impl Drop for NcursesWindow {
     fn drop(&mut self) {
         endwin();
+    }
+}
+
+impl NcursesWindow {
+    pub fn newwin(lines: i32, cols: i32, y: i32, x: i32) -> Self {
+        NcursesWindow(newwin(lines, cols, y, x))
+    }
+    pub fn addstr(&self, s: &str) {
+        waddstr(self.0, s);
+    }
+    pub fn getch(&self) -> i32 {
+        wgetch(self.0)
+    }
+    pub fn getcurx(&self) -> i32 {
+        getcurx(self.0)
+    }
+    pub fn getcury(&self) -> i32 {
+        getcury(self.0)
+    }
+    pub fn keypad(&self, b: bool) -> i32 {
+        keypad(self.0, b)
+    }
+    pub fn move_(&self, y: i32, x: i32) -> i32 {
+        wmove(self.0, y, x)
+    }
+    pub fn noecho(&self) -> i32 {
+        noecho()
+    }
+    pub fn raw(&self) -> i32 {
+        raw()
+    }
+    pub fn refresh(&self) -> i32 {
+        wrefresh(self.0)
     }
 }
