8abdd69b3cad45b139a695dc5d01b13fa74d0d24
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-27 14:37:54 -0400

    working on

--- rust/src/utils/ncurses.rs
+++ rust/src/utils/ncurses.rs
@@ -1,24 +1,51 @@
+use std::rc::Rc;
+
 use ncurses::*;
 
+use crate::utils::*;
+
+pub use ncurses::{ERR, OK};
 pub use ncurses::{KEY_F0, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F9};
 pub use ncurses::{KEY_F10, KEY_F11, KEY_F12, KEY_F13, KEY_F14, KEY_F15};
 
+#[cfg(debug_assertions)]
+#[derive(Debug)]
+pub struct NcursesWindow(Rc<WINDOW>);
+
+#[cfg(not(debug_assertions))]
 #[derive(Debug)]
 pub struct NcursesWindow(WINDOW);
 
 impl Default for NcursesWindow {
+    #[cfg(debug_assertions)]
     fn default() -> Self {
-        NcursesWindow(initscr())
+        NcursesWindow(Rc::new(stdscr()))
+    }
+    #[cfg(not(debug_assertions))]
+    fn default() -> Self {
+        NcursesWindow(stdscr())
     }
 }
 
 impl Drop for NcursesWindow {
+    #[cfg(debug_assertions)]
     fn drop(&mut self) {
-        endwin();
+        if Rc::strong_count(&self.0) == 1 {
+            endwin();
+        }
+    }
+    #[cfg(not(debug_assertions))]
+    fn drop(&mut self) {
+        if (self.0 == stdscr()) {
+            endwin()
+        }
     }
 }
 
 impl NcursesWindow {
+    pub fn get_window(&self) -> WINDOW {
+        self.0
+    }
     pub fn newwin(lines: i32, cols: i32, y: i32, x: i32) -> Self {
         NcursesWindow(newwin(lines, cols, y, x))
     }
@@ -31,6 +58,12 @@ impl NcursesWindow {
     pub fn getch(&self) -> i32 {
         wgetch(self.0)
     }
+    pub fn getbegx(&self) -> i32 {
+        getbegx(self.0)
+    }
+    pub fn getbegy(&self) -> i32 {
+        getbegy(self.0)
+    }
     pub fn getcurx(&self) -> i32 {
         getcurx(self.0)
     }
@@ -42,6 +75,9 @@ impl NcursesWindow {
     }
     pub fn getmaxy(&self) -> i32 {
         getmaxy(self.0)
+    }
+    pub fn halfdelay(&self, tenths: i32) -> i32 {
+        halfdelay(tenths)
     }
     pub fn keypad(&self, b: bool) -> i32 {
         keypad(self.0, b)
