c74d69a05b993ec027920be3e063ecc94e2b258e
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-28 01:25:27 -0400

    upd cmake

--- cpp/.cmake_config/flags/debug.cmake
+++ cpp/.cmake_config/flags/debug.cmake
@@ -7,8 +7,7 @@ include(${__CFG__}/try-add-flag.cmake)
 try_add_flag(CMAKE_CXX_FLAGS_DEBUG -Og)
 try_add_flag(CMAKE_CXX_FLAGS_DEBUG -g3)
 
-try_add_flag(CMAKE_CXX_FLAGS_DEBUG -D_FORTIFY_SOURCE=3)
-try_add_flag(CMAKE_CXX_FLAGS_DEBUG -D_GLIBCXX_DEBUG)
+add_compile_definitions(-D_FORTIFY_SOURCE=3 -D_GLIBCXX_DEBUG)
 
 try_add_flag(CMAKE_CXX_FLAGS_DEBUG -fcf-protection=full)
 try_add_flag(CMAKE_CXX_FLAGS_DEBUG -fharden-compares)
