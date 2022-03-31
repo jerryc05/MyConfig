331ed5ba1310ff6310646e5eaa53404b71ac9dfd
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-31 17:59:40 -0400

    upd jemalloc

---
+++ os/jemalloc/setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+echo 'Update date: 2022-03-31'
+set -eou pipefail
+
+# Install `git`, `autoconf`
+sudo apt install git autoconf || \
+sudo pacman -S   git autoconf
+
+JEMALLOC='jemalloc'
+JEMALLOC_DIR="`pwd`/$JEMALLOC"
+(
+  # Clone/update `jemalloc`
+  [ -d "$JEMALLOC_DIR" ] || git clone --depth=1 "https://github.com/$JEMALLOC/$JEMALLOC.git" "$JEMALLOC_DIR"
+  cd "$JEMALLOC_DIR"
+  git fetch --depth=1
+  git reset --hard FETCH_HEAD
+
+  # Build `jemalloc`
+  export EXTRA_CFLAGS="-DNDEBUG -Ofast -march=native -w"
+  export EXTRA_CXXFLAGS="$EXTRA_CFLAGS"
+  ./autogen.sh \
+  --disable-stats \
+  --disable-prof-libgcc \
+  --disable-prof-gcc \
+  --disable-fill \
+  --enable-lazy-lock
+
+  MAKEFLAGS="$MAKEFLAGS -j `nproc`"
+  make
+  sudo make install
+)
