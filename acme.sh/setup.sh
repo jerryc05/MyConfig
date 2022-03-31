90daf2a8e5042c63406672bd75306f4cb1978707
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-03-31 02:15:43 -0400

    upd acme.sh

--- acme.sh/setup.sh
+++ acme.sh/setup.sh
@@ -6,25 +6,36 @@ set -eou pipefail
 read -p "Enter email address: " YOUR_EMAIL_ADDR
 read -p "Enter domain: " YOUR_DOMAIN
 
-INSTALL_PATH='/etc/acme.sh'
+DEPLOY_PATH='/etc/acme.sh'
+INSTALL_PATH="${XDG_CONFIG_HOME:="$HOME/.config"}/acme.sh"
 KEY_LEN='ec-256'
 
 # Pre-install
-sudo mkdir -p -m755 "$INSTALL_PATH"
+sudo mkdir -p "$DEPLOY_PATH"
+sudo chmod -R 755 "$DEPLOY_PATH"
+mkdir -p "$INSTALL_PATH"
 
-# Clone/update repo
+REPO_NAME='acme.sh'
 (
-  REPO_NAME='acme.sh'
+  # Clone/update repo
   [ -d "$REPO_NAME" ] || git clone --depth=1 "https://github.com/acmesh-official/$REPO_NAME.git"
   cd "$REPO_NAME"
   git fetch --depth=1
   git reset --hard FETCH_HEAD
+
+  # Install
+  ./acme.sh --install --cert-home "$INSTALL_PATH" --keylength "$KEY_LEN" -m "$YOUR_EMAIL_ADDR"
 )
 
-# Install
-"$REPO_NAME/acme.sh" --install --cert-home "$INSTALL_PATH" --keylength "$KEY_LEN" -m "$YOUR_EMAIL_ADDR"
+# Reload shell
+. "$HOME/.`echo $0 | sed 's/^-//'`rc"
 
 # Issue
 WWW_ROOT='/var/wwwroot'  # also change in [nginx.conf]
-sudo mkdir -p -m777 "$WWW_ROOT"
-acme.sh --issue --cron --keylength "$KEY_LEN" -d "$YOUR_DOMAIN" -w "$WWW_ROOT"
+sudo mkdir -p "$WWW_ROOT"
+sudo chmod -R 777 "$WWW_ROOT"
+acme.sh --issue --keylength "$KEY_LEN" -d "$YOUR_DOMAIN" -w "$WWW_ROOT"
+
+# Deploy
+chmod -R 755 "$INSTALL_PATH"
+sudo mv "$INSTALL_PATH" "$DEPLOY_PATH"
