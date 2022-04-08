5cc69c05263dda14ce8f9596797bbb2b742870a5
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-04-08 17:37:44 -0400

    upd os

--- os/network/other-sysctl/setup.sh
+++ os/network/other-sysctl/setup.sh
@@ -5,5 +5,8 @@ set -eou pipefail
 
 . `dirname "$0"`/../util.sh
 
-tee_if_not_exists 'net.ipv4.tcp_fastopen = 3' '/etc/sysctl.conf'
-tee_if_not_exists 'net.ipv4.tcp_notsent_lowat = 16384  # 16KiB'
+tee_if_not_exists 'net.ipv4.tcp_fastopen = 3'                   '/etc/sysctl.conf'
+tee_if_not_exists 'net.ipv4.tcp_tw_reuse = 1'                   '/etc/sysctl.conf'
+tee_if_not_exists 'net.ipv4.tcp_notsent_lowat = 16384  # 16KiB' '/etc/sysctl.conf'
+
+sudo sysctl -p
