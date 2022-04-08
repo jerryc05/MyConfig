139c5cea5b92a98832a04507e8f81bbf88a4b59b
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-04-08 17:54:47 -0400

    add keepalive time

--- os/network/other-sysctl/setup.sh
+++ os/network/other-sysctl/setup.sh
@@ -5,6 +5,7 @@ set -eou pipefail
 
 . `dirname "$0"`/../util.sh
 
+tee_if_not_exists 'net.ipv4.tcp_keepalive_time = 1200'          '/etc/sysctl.conf'
 tee_if_not_exists 'net.ipv4.tcp_fastopen = 3'                   '/etc/sysctl.conf'
 tee_if_not_exists 'net.ipv4.tcp_tw_reuse = 1'                   '/etc/sysctl.conf'
 tee_if_not_exists 'net.ipv4.tcp_notsent_lowat = 16384  # 16KiB' '/etc/sysctl.conf'
