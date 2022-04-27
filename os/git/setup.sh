d1c9ee49a06cdf6257b53f90aa61b2100808c05f
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-04-27 18:20:46 -0400

    upd git config

--- os/git/setup.sh
+++ os/git/setup.sh
@@ -2,20 +2,29 @@
 
 git config --global user.name   'Ziyan "Jerry" Chen'
 git config --global user.email  'jerryc443@gmail.com'
-git config --get-regexp 'user.*'
+
 
 git config --global core.autocrlf   input
 git config --global core.eol        lf
 git config --global core.fileMode   true
-git config --global core.safecrlf   warn
+git config --global core.fsmonitor  true
 git config --global core.longpaths  true
+git config --global core.safecrlf   warn
 #git config --global core.sshcommand "C:/Windows/System32/OpenSSH/ssh.exe"
 #  [core.sshcommand] will be overridden by [GIT_SSH_COMMAND] env var
-git config --get-regexp 'core.*'
+
+
+git config --global core.symlinks            true
+git config --global help.autoCorrect         prompt
+git config --global push.recursesubmodules   check
+git config --global pull.rebase              false
+git config --global rebase.autoStash         true
+git config --global merge.autoStash          true
+
 
 git config --global rerere.enabled    true
 #git config --global rerere.autoUpdate true
-git config --get-regexp 'rerere.*'
+
 
 git config --global commit.gpgsign         true
 git config --global tag.gpgsign            true
@@ -27,14 +36,6 @@ git config --global gpg.ssh.defaultKeyCommand 'ssh-add -L'
 #git config --global gpg.program "/path/to/gpg_executable"
 #git config --global user.signingkey GPG_KEY_ID_OR_SSH_PUBKEY_HERE
 
-git config --global init.defaultBranch       main
-git config --global push.recursesubmodules   check
-git config --global core.usebuiltinfsmonitor true
-git config --global core.symlinks            true
-
-git config --global pull.rebase      true
-git config --global rebase.autoStash true
-git config --global help.autoCorrect prompt
 
 git config --global alias.c 'commit'
 git config --global alias.p '!git pull && git push'
