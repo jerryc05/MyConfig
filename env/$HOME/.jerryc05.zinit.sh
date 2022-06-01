1ade16f07879820af00adf8aa7be1dd679b7992d
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-06-01 12:39:08 -0400

    Update .jerryc05.zinit.sh

--- env/$HOME/.jerryc05.zinit.sh
+++ env/$HOME/.jerryc05.zinit.sh
@@ -3,9 +3,10 @@
 # Plugins
 zinit ice depth=1;zinit light zsh-users/zsh-autosuggestions && export ZSH_AUTOSUGGEST_USE_ASYNC=1
 #zinit ice depth=1;zinit light zsh-users/zsh-completions
-zinit ice depth=1;zinit light marlonrichert/zsh-autocomplete \
-&& zstyle ':completion:*:*:man:*:*' menu select=long search
-#  └ https://github.com/marlonrichert/zsh-autocomplete/issues/388#issuecomment-1010988568
+zinit ice depth=1;zinit light marlonrichert/zsh-autocomplete #\
+#&& zstyle ':completion:*' substitute no && zstyle ':completion:*:*:man:*:*' menu select=long search
+#   |                                       └ https://github.com/marlonrichert/zsh-autocomplete/issues/388#issuecomment-1010988568
+#   └ https://github.com/marlonrichert/zsh-autocomplete/issues/374
 zinit ice depth=1;zinit light agkozak/zsh-z
 zinit ice depth=1;zinit light supercrabtree/k && command -v numfmt >/dev/null || { command -v brew >/dev/null && brew install coreutils }
 
