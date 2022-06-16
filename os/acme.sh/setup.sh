3673336e5120f1cd9c7cc73ca4724760870c237a
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-06-16 01:46:50 -0400

    upd acme and nginx

--- os/acme.sh/setup.sh
+++ os/acme.sh/setup.sh
@@ -2,20 +2,24 @@
 
 set -eou pipefail
 
-[ "$EUID" -ne 0 ] && echo "Please run as root" && exit
+[ "$EUID" -ne 0 ] && echo 'ERR: root privilege needed!' && false
 
-read -p 'Enter email address: '                        YOUR_EMAIL_ADDR
-read -p 'Enter domain, unquoted and separated by [-d]: ' YOUR_DOMAIN
+command -v git &>/dev/null && echo 'ERR: git not found!' && false
 
-# Install `git`
-sudo apt install git || \
-sudo pacman -S   git
+read -p 'Enter email address: '                               EMAIL_ADDR
+read -p 'Enter main domain (only one): '                      MAIN_DOMAIN
+read -p 'Enter SAN domains, unquoted and separated by [-d]: ' SAN_DOMAINS
+read -p 'Enter hook commands to be exec after renew: '        HOOK_CMD
+
+[[ "$MAIN_DOMAIN" =~ ' ' ]] && echo 'ERR: main domain cannot contain SPACE!' && false
 
 # Pre-install
 KEY_LEN='ec-256'
-DEPLOY_PATH='/etc/acme.sh' 
-sudo mkdir -p "$DEPLOY_PATH"
-sudo chmod -R 766 "$DEPLOY_PATH"
+ACME_HOME="$HOME/.acme.sh"
+
+DEPLOY_PATH='/etc/acme.sh'
+mkdir -p "$DEPLOY_PATH"
+chmod -R 755 "$DEPLOY_PATH"
 
 REPO_NAME='acme.sh'
 (
@@ -25,20 +29,27 @@ REPO_NAME='acme.sh'
   git fetch --depth=1
   git reset --hard origin/HEAD
 
-  # Install
-  ./acme.sh --install --cert-home "$DEPLOY_PATH" --keylength "$KEY_LEN" -m "$YOUR_EMAIL_ADDR"
+  # Fake install
+  ln -s "`PWD`" "$ACME_HOME"
+  [ `readlink -f "$ACME_HOME"` != "`pwd`" ] && echo "ERR: Fake install to [$ACME_HOME] failed!" && false
 )
 
-# Reload shell
-. "$HOME/.`echo $0 | sed 's/^-//'`rc"
+(
+  cd "$ACME_HOME"
 
-# Issue
-WWW_ROOT='/var/wwwroot'  # also change in [nginx.conf]
-sudo mkdir -p "$WWW_ROOT"
-sudo chmod -R 777 "$WWW_ROOT"
-acme.sh --issue --keylength "$KEY_LEN" -d $YOUR_DOMAIN -w "$WWW_ROOT"
+  # Set default CA
+  ./acme.sh --set-default-ca --server buypass
 
-# Deploy
-chmod -R +r "$INSTALL_PATH"
-find "$INSTALL_PATH" -type d | xargs chmod +x
-sudo mv "$INSTALL_PATH/acme.sh" "$DEPLOY_PATH"
+  # Register
+  ./acme.sh --register-account -m "$EMAIL_ADDR"
+
+  # Issue
+  WWW_ROOT='/var/wwwroot'  # also change in [nginx.conf]
+  mkdir -p "$WWW_ROOT"
+  chmod -R 777 "$WWW_ROOT"
+  ./acme.sh --issue --days 170 --keylength "$KEY_LEN" -d "$MAIN_DOMAIN" -d $SAN_DOMAINS -w "$WWW_ROOT"
+  #                        └ buypass cert has 180 days
+
+  # Auto renew
+  crontab <<< "0 0 * * 0 `pwd`/acme.sh --cron && ${HOOK_CMD:-true}"
+)
