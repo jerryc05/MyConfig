b2c9a5d5fc6c2b20bede5f4d6b6d5304ea46d16b
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-08-04 03:31:47 -0400

    Update setup.sh

--- os/network/other-sysctl/setup.sh
+++ os/network/other-sysctl/setup.sh
@@ -9,6 +9,7 @@ tee_if_not_exists 'net.ipv4.tcp_fastopen = 3'          '/etc/sysctl.conf'
 tee_if_not_exists 'net.ipv4.tcp_tw_reuse = 1'          '/etc/sysctl.conf'
 tee_if_not_exists '# 16384 = 16KiB'                    '/etc/sysctl.conf'
 tee_if_not_exists 'net.ipv4.tcp_notsent_lowat = 16384' '/etc/sysctl.conf'
+tee_if_not_exists '# Increase to benefit from QUIC'    '/etc/sysctl.conf'
 tee_if_not_exists 'net.core.rmem_max=2500000'          '/etc/sysctl.conf'
 
 
