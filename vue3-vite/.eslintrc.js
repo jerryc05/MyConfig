3d336208cfc2ddfd3f51f867f0e18af86d522177
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-08-21 16:21:42 -0400

    Update .eslintrc.js

--- vue3-vite/.eslintrc.js
+++ vue3-vite/.eslintrc.js
@@ -111,6 +111,7 @@ module.exports = {
     'no-magic-numbers': 'off',
     'no-ternary': 'off',
     'object-curly-spacing': 'off',
+    'one-var': 'off',
     'quote-props': 'off',
     'sort-keys': 'off',
     'sort-vars': 'off',
