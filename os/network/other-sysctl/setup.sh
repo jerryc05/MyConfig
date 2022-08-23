1a94d82eb56c495f7fcfd42aacbd3b89be34b31d
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-08-23 00:55:50 -0400

    Update setup.sh

--- os/network/other-sysctl/setup.sh
+++ os/network/other-sysctl/setup.sh
@@ -11,6 +11,8 @@ tee_if_not_exists '# 16384 = 16KiB'                    '/etc/sysctl.conf'
 tee_if_not_exists 'net.ipv4.tcp_notsent_lowat = 16384' '/etc/sysctl.conf'
 tee_if_not_exists '# Increase to benefit from QUIC'    '/etc/sysctl.conf'
 tee_if_not_exists 'net.core.rmem_max=2500000'          '/etc/sysctl.conf'
+tee_if_not_exists '# Allow anyone to use [ping]'           '/etc/sysctl.conf'
+tee_if_not_exists 'net.ipv4.ping_group_range=0 2147483647' '/etc/sysctl.conf'
 
 
 sudo sysctl -p
