c8e277374ebd811a1d3b77ab70c7ef8211ed85ae
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-08-31 17:55:00 -0400

    Update App.vue

--- vue3-vite/src/App.vue
+++ vue3-vite/src/App.vue
@@ -29,4 +29,12 @@
 .flex-center {
   @apply justify-center items-center;
 }
+
+::-webkit-scrollbar {
+  @apply w-4;
+}
+
+::-webkit-scrollbar-thumb {
+  @apply rounded-xl border-4 border-solid border-transparent bg-clip-content bg-gray-500;
+}
 </style>
