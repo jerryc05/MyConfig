8d5b17d061261d4d4a05652db2fc2b12b8e0e4c7
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-11-06 14:53:38 -0500

    Update bbr.sh

--- os/network/bbr.sh
+++ os/network/bbr.sh
@@ -6,11 +6,11 @@ set -eou pipefail
 
 sudo modprobe tcp_bbr
 tee_if_not_exists 'tcp_bbr' '/etc/modules-load.d/bbr.conf'
-lsmod | fgrep bbr
-sysctl net.ipv4.tcp_available_congestion_control | fgrep bbr
+lsmod | grep -F bbr
+sysctl net.ipv4.tcp_available_congestion_control | grep -F bbr
 
 tee_if_not_exists 'net.ipv4.tcp_congestion_control = bbr' '/etc/sysctl.conf'
 tee_if_not_exists '# BBR must be used with fq qdisc'      '/etc/sysctl.conf'
 tee_if_not_exists 'net.core.default_qdisc = fq'           '/etc/sysctl.conf'
 sudo sysctl -p
-sysctl net.ipv4.tcp_congestion_control | fgrep bbr
+sysctl net.ipv4.tcp_congestion_control | grep -F bbr
