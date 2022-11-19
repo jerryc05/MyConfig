c8e77b5bb4b23c40f842dc42566a09a15370910b
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-11-19 02:29:44 -0500

    Update and rename solidjs.sh to solidjs.py

---
+++ vite-ts/solidjs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import subprocess as sp
+import json
+
+sp.check_call("npx degit solidjs/templates/ts ./".split(" "))
+
+
+sp.check_call("pnpm up -Lri".split(" "))
+#                       ||└ interactive
+#                       |└- recursive
+#                       └-- update to latest version
+
+sp.check_call("pnpm i @babel/core @babel/preset-env babel-preset-solid -D".split(" "))
+
+with open(".bablerc", "w", encoding="utf-8") as f:
+    f.write(
+        json.dumps({"presets": ["solid", ["@babel/preset-env", {"bugfixes": True}]]})
+    )
+
+with open(".browserslistrc", "w", encoding="utf-8") as f:
+    f.write(
+        """
+last 2 chrome version
+last 2 firefox version
+last 2 and_chr version
+last 2 and_ff version
+last 2 ios_saf version
+"""
+    )
+
+with open("src/index.tsx", "r+", encoding="utf-8") as f:
+    content = f.read()
+    content = (
+        content[: content.find("render(() => <App />, document.getElementById('root')")]
+        + """
+const mount = document.createElement('div')
+document.body.insertBefore(mount, document.body.firstChild)
+render(() => <App />, mount)
+"""
+    )
+    f.truncate(0)
+    f.seek(0)
+    f.write(content)
