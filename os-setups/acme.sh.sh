404d51c9b3e21af25c6228c5db954bac12a29e09
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-01-10 19:51:07 -0500

    Update acme.sh.sh

--- os-setups/acme.sh.sh
+++ os-setups/acme.sh.sh
@@ -3,26 +3,26 @@
 set -eou pipefail
 
 [ "$EUID" -ne 0 ] && echo 'ERR: root privilege needed!' && false
-
 command -v git &>/dev/null && echo 'ERR: git not found!' && false
+
 
 read -p 'Enter email address: ' EMAIL_ADDR
 
 read -p 'Enter main domain (only one): '                      MAIN_DOMAIN
+[[ "$MAIN_DOMAIN" =~ ' ' ]] && echo 'ERR: main domain cannot contain SPACE!' && false
+
 read -p 'Enter SAN domains, unquoted and separated by [-d]: ' SAN_DOMAINS
 [ -n "$SAN_DOMAINS" ] && D_SAN_DOMAINS="-d $SAN_DOMAINS" || D_SAN_DOMAINS="$SAN_DOMAINS"
 
 read -p 'Enter hook commands to be exec after renew (e.g. `systemctl restart nginx`): ' HOOK_CMD
 
-[[ "$MAIN_DOMAIN" =~ ' ' ]] && echo 'ERR: main domain cannot contain SPACE!' && false
 
 # Pre-install
-KEY_LEN='ec-256'
-ACME_HOME="$HOME/.acme.sh"
+KEY_LEN='ec-256' 
 
 DEPLOY_PATH='/etc/acme.sh'
-mkdir -p "$DEPLOY_PATH"
-chmod -R 755 "$DEPLOY_PATH"
+[ -d "$DEPLOY_PATH" ] || mkdir -p -m755 "$DEPLOY_PATH"
+
 
 REPO_NAME='acme.sh'
 (
@@ -32,39 +32,33 @@ REPO_NAME='acme.sh'
   git fetch --depth=1
   git reset --hard origin/HEAD
 
-  # Fake install
-  ln -s "`pwd`" "$ACME_HOME"
-  [ `readlink -f "$ACME_HOME"` != "`pwd`" ] && echo "ERR: Fake install to [$ACME_HOME] failed\!" && false
-)
-
-(
-  cd "$ACME_HOME"
-
-  # Set default CA
-  ./acme.sh --set-default-ca --server buypass
-
-  # Register
-  ./acme.sh --register-account -m "$EMAIL_ADDR"
-
-  # Issue
-  WWW_ROOT='/var/wwwroot'  # also change in [nginx.conf]
-  mkdir -p "$WWW_ROOT"
-  chmod -R 777 "$WWW_ROOT"
-  chcon -Rt httpd_sys_content_t "$WWW_ROOT" || true
-  ./acme.sh --issue --days 170 --keylength "$KEY_LEN" -d "$MAIN_DOMAIN" $D_SAN_DOMAINS -w "$WWW_ROOT"
-  #                        └ buypass cert has 180 days
-
   # Install
-  INSTALL_CMD=`
-  echo \
-  acme.sh --install-cert -d "$MAIN_DOMAIN" \
-          --cert-file      "$DEPLOY_PATH/cert.pem" \
-          --key-file       "$DEPLOY_PATH/key.pem" \
-          --fullchain-file "$DEPLOY_PATH/fullchain.pem"
-  `
-
-  $INSTALL_CMD
-  
-  # Auto renew
-  crontab <<< "0 0 * * * `pwd`/acme.sh --cron && { ${INSTALL_CMD:-true} ; } && ${HOOK_CMD:-true}"
+  ./acme.sh --install -m $EMAIL_ADDR
 )
+ 
+[[ "$0" = *bash ]] && . ~/.bashrc
+[[ "$0" =  *zsh ]] && .  ~/.zshrc
+
+# Set default CA
+#./acme.sh --set-default-ca --server letsencrypt 
+
+# Issue
+WWW_ROOT='/var/wwwroot'  # also change in [nginx.conf]
+mkdir -p "$WWW_ROOT"
+chmod -R 777 "$WWW_ROOT"
+chcon -Rt httpd_sys_content_t "$WWW_ROOT" || true
+./acme.sh --issue --keylength "$KEY_LEN" -d "$MAIN_DOMAIN" $D_SAN_DOMAINS -w "$WWW_ROOT"
+
+# Install
+#INSTALL_CMD=`
+#echo \
+#acme.sh --install-cert -d "$MAIN_DOMAIN" \
+#        --cert-file      "$DEPLOY_PATH/cert.pem" \
+#        --key-file       "$DEPLOY_PATH/key.pem" \
+#        --fullchain-file "$DEPLOY_PATH/fullchain.pem"
+#`
+
+#$INSTALL_CMD
+
+# Auto renew
+#crontab <<< "0 0 * * * `pwd`/acme.sh --cron && { ${INSTALL_CMD:-true} ; } && ${HOOK_CMD:-true}"
