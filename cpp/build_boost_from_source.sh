23d0de311dbb231bbb5b16c67d76930de0e24b43
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-02-17 17:58:13 -0500

    add boost

---
+++ cpp/build_boost_from_source.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+git clone --single-branch --branch master --depth=1 --recurse-submodules --shallow-submodules https://github.com/boostorg/boost.git
+# git clone --single-branch --branch master --depth=1 https://github.com/boostorg/boost.git
+cd boost
+# git submodule update --init --recursive --remote --no-fetch --depth=1
+
+
+export CFLAGS="-march=native -Ofast"
+export CXXFLAGS="$CFLAGS"
+./bootstrap.sh --with-python=`which python3`
+./b2 -j`nproc` 
+
+
+echo "export BOOST_ROOT=`pwd`" >>~/.bashrc  # for cmake
+
+echo "export CPATH=`pwd`:${CPATH}" >>~/.bashrc
+echo "export LIBRARY_PATH=`pwd`/stage/lib:${LIBRARY_PATH}" >>~/.bashrc
