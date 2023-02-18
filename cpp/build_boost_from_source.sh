2b85a1ca7792fc98d28348f9a9ce6698a81f1fdd
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-02-17 21:26:53 -0500

    boost

--- cpp/build_boost_from_source.sh
+++ cpp/build_boost_from_source.sh
@@ -2,15 +2,16 @@
 
 set -euxo pipefail
 
-git clone --single-branch --branch master --depth=1 --recurse-submodules --shallow-submodules https://github.com/boostorg/boost.git
 # git clone --single-branch --branch master --depth=1 https://github.com/boostorg/boost.git
+git clone --single-branch --branch master --depth=1 --recurse-submodules --shallow-submodules https://github.com/boostorg/boost.git
 cd boost
 # git submodule update --init --recursive --remote --no-fetch --depth=1
 
 
 export CFLAGS="-w -march=native -Ofast"
 export CXXFLAGS="$CFLAGS"
-./bootstrap.sh --with-python=`which python3`
+# ./bootstrap.sh --with-python=`which python3` --with-icu
+./bootstrap.sh --with-icu --without-libraries=python,mpi
 ./b2 -j`nproc` cflags="$CFLAGS" cxxflags="$CXXFLAGS"
 
 
