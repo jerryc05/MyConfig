6320f5173a8eb1912831830c9c3c0dbee04758cc
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-02-27 23:22:19 -0500

    boost

--- cpp/build_boost_from_source.sh
+++ cpp/build_boost_from_source.sh
@@ -2,25 +2,35 @@
 
 set -euxo pipefail
 
-if [ -d boost ]; then
-    # git clone --single-branch --branch master --depth=1 https://github.com/boostorg/boost.git
-    git clone --single-branch --branch master --depth=1 --recurse-submodules --shallow-submodules https://github.com/boostorg/boost.git
-    cd boost
-    # git submodule update --init --recursive --remote --no-fetch --depth=1
-else
-    cd boost
-    git submodule foreach | cut -d ' ' -f 2 | xargs -I@ -P `nproc` sh -c 'cd @ && git fetch --depth 1 && git reset --hard FETCH_HEAD'    
-fi
-
-export CFLAGS="-w -march=native -Ofast"
-export CXXFLAGS="$CFLAGS"
-./bootstrap.sh --with-python=`which python3` --with-icu --without-libraries=mpi
-./b2 -j`nproc` cflags="$CFLAGS" cxxflags="$CXXFLAGS"
+build() {
+    export CFLAGS="-w -march=native -Ofast -std=gnu++20"
+    export CXXFLAGS="$CFLAGS"
+    ./bootstrap.sh --with-python=`which python3` --with-icu --without-libraries=mpi
+    ./b2 -j`nproc` cflags="$CFLAGS" cxxflags="$CXXFLAGS"
+}
 
 
-echo -e '\n\n' >>~/.bashrc
 
-echo "export BOOST_ROOT=`pwd`  # for cmake" >>~/.bashrc
+###### THIS IS FOR NEW INSTALLATION
+git clone --single-branch --branch master --depth=1 --recurse-submodules --shallow-submodules https://github.com/boostorg/boost.git
+cd boost
 
-echo "export CPATH=`pwd`:\$CPATH" >>~/.bashrc
-echo "export LIBRARY_PATH=`pwd`/stage/lib:\$LIBRARY_PATH" >>~/.bashrc
+build
+
+cat <<<EOF >>~/.bashrc
+
+
+export BOOST_ROOT=`pwd`  # for cmake" >>~/.bashrc
+
+export CPATH=`pwd`:\$CPATH
+export LIBRARY_PATH=`pwd`/stage/lib:\$LIBRARY_PATH
+
+EOF
+
+
+
+###### THIS IS FOR UPDATE
+cd boost
+git submodule foreach | cut -d ' ' -f 2 | xargs -I@ -P `nproc` sh -c 'cd @ && git fetch --depth 1 && git reset --hard FETCH_HEAD'    
+
+build
