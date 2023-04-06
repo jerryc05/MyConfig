6849129f5d08be613adb838f16e7a5d57101838b
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-04-06 18:00:43 -0400

    sqlite

--- sql/sqlite/schema.sql
+++ sql/sqlite/schema.sql
@@ -1,8 +1,26 @@
-pragma synchronous = off;      -- off | normal | full
-pragma journal_mode = memory;  -- off | memory | persist | truncate | wal | delete
+-- ONCE
+pragma synchronous = off;      -- off | normal | full; use [normal] when serious
+pragma journal_mode = memory;  -- off | memory | persist | truncate | wal | delete; use [wal] when serious
 pragma temp_store = memory;
-pragma foreign_keys = on;
--- sqlite3 db.sqlite3 "vacuum"
+pragma auto_vacuum = incremental;
+pragma mmap_size = 17179869184; -- 16 GiB
+pragma page_size = 16384;       -- 16 KiB
 
+-- -- Think before doing this
+pragma foreign_keys = on;
+
+
+
+-- REGULARLY
+pragma vacuum;  -- VERY EXPENSIVE!!! Or use incremental version below
+
+pragma incremental_vacuum;  -- Must have [auto_vacuum = incremental]
+
+pragma optimize;    -- it is recommended that applications run "PRAGMA optimize" (with no arguments) just before closing each database connection. 
+                    -- Long-running applications might also benefit from setting a timer to run "PRAGMA optimize" every few hours.
+
+
+
+-- -- Visual improvements
 .headers on
 .mode column
