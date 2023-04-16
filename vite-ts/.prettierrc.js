326bc5239401faffbcad0fdf6e8de312679be2da
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-04-16 19:25:02 -0400

    prettier

--- vite-ts/.prettierrc.js
+++ vite-ts/.prettierrc.js
@@ -3,18 +3,17 @@ pnpm i -D prettier @trivago/prettier-plugin-sort-imports
 */
 
 const addSortImport = cfg => ({
-    ...cfg,
-    importOrder: ['^@/', '^./'],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
-    plugins: [...(cfg.plugins ?? []), '@trivago/prettier-plugin-sort-imports'],
-  })
-  
-  module.exports = addSortImport({
-    semi: false,
-    singleQuote: true,
-    jsxSingleQuote: true,
-    arrowParens: 'avoid',
-    vueIndentScriptAndStyle: true,
-  })
-  
+  ...cfg,
+  importOrder: ['^@/', '^./'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [...(cfg.plugins ?? []), '@trivago/prettier-plugin-sort-imports'],
+})
+
+module.exports = addSortImport({
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+  vueIndentScriptAndStyle: true,
+})
