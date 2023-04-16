5afd4a98bcbd64a2a547138e77c80e3243f40615
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-04-16 01:08:35 -0400

    boost

--- cpp/build_boost_from_source.sh
+++ cpp/build_boost_from_source.sh
@@ -5,18 +5,16 @@ set -euxo pipefail
 build() {
     export CFLAGS="-w -march=native -Ofast -std=gnu++20"
     export CXXFLAGS="$CFLAGS"
-    ./bootstrap.sh --with-python=`which python3` --with-icu --without-libraries=mpi
+    ./bootstrap.sh --with-python=`which python3` --with-icu #--without-libraries=mpi
     ./b2 -j`nproc` cflags="$CFLAGS" cxxflags="$CXXFLAGS"
 }
 
+mkdir boost && cd boost
 
 
 ###### THIS IS FOR NEW INSTALLATION ######
-git clone --single-branch --branch master --depth=1 --recurse-submodules --shallow-submodules https://github.com/boostorg/boost.git
-cd boost
-
+git clone --single-branch --branch master --depth=1 --recurse-submodules --shallow-submodules https://github.com/boostorg/boost.git ./
 build
-
 cat <<EOF >>~/.bashrc
 
 # boost
@@ -29,7 +27,5 @@ EOF
 
 
 ###### THIS IS FOR UPDATE ######
-cd boost
 git submodule foreach | cut -d ' ' -f 2 | xargs -I@ -P `nproc` sh -c 'cd @ && git fetch --depth 1 && git reset --hard FETCH_HEAD'    
-
 build
