368318852b7918003f8a35346162e52eee1c6a5d
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-08-14 00:33:38 -0400

    upd

--- rust/my-release-build.sh
+++ rust/my-release-build.sh
@@ -1,4 +1,4 @@
-RUSTFLAGS="$RUSTFLAGS -C opt-level=3 -C inline-threshold=1000 -C target-cpu=native -C debug-assertions=false -C overflow-checks=false -C linker-plugin-lto=true -C incremental=true -C codegen-units=256" cargo build --timings -Z build-std --target="$(rustc -vV|sed -n "s|host: ||p")"
+RUSTFLAGS="$RUSTFLAGS -C opt-level=3 -C inline-threshold=1000 -C target-cpu=native -C debug-assertions=false -C overflow-checks=false -C linker-plugin-lto=true -C incremental=true -C codegen-units=256" cargo build --release -Z build-std --target="$(rustc -vV|sed -n "s|host: ||p")"
 
 
 # -Zbuild-std=std,panic_abort -Zbuild-std-features=panic_immediate_abort 
