41ca4f3bc7be6ec8e001ce7a09f33a9a625ee622
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-08-14 02:11:57 -0400

    upd

--- rust/my-release-build.sh
+++ rust/my-release-build.sh
@@ -1,4 +1,7 @@
-RUSTFLAGS="$RUSTFLAGS -C opt-level=3 -C inline-threshold=1000 -C target-cpu=native -C debug-assertions=false -C overflow-checks=false -C linker-plugin-lto=true -C incremental=true -C codegen-units=256" cargo build --release -Z build-std --target="$(rustc -vV|sed -n "s|host: ||p")"
+RUSTFLAGS="$RUSTFLAGS -C opt-level=3 -C inline-threshold=1000 -C target-cpu=native -C debuginfo=0 -C strip=symbols -C debug-assertions=false -C overflow-checks=false -C lto=true -C linker-plugin-lto=true -C link-arg=-fuse-ld=lld -C incremental=true -C codegen-units=256" cargo build --release -Z build-std --target="$(rustc -vV|sed -n "s|host: ||p")"
+
+
+RUSTFLAGS="$RUSTFLAGS -C opt-level=3 -C inline-threshold=1000 -C target-cpu=native -C debuginfo=0 -C strip=symbols -C debug-assertions=false -C overflow-checks=false -C link-arg=-fuse-ld=lld -C incremental=true -C codegen-units=1" cargo build --release -Z build-std --target="$(rustc -vV|sed -n "s|host: ||p")"
 
 
 # -Zbuild-std=std,panic_abort -Zbuild-std-features=panic_immediate_abort 
