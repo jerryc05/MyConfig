823c937cc1955edfe875d3f1724001c05cdeaa29
jerryc05 <jerryc443@gmail.com>
2024-10-20 12:41:08 -0700

    brew

--- os-setups/brew.sh
+++ os-setups/brew.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-export HOMEBREW_INSTALL_FROM_API=1
+# export HOMEBREW_INSTALL_FROM_API=1
+# ^^^^^^ default since brew 4.0
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 
 brew doctor
