2ff2c844f6158565aa0d097852af74bdfc43e5c6
jerryc05 <jerryc443@gmail.com>
2024-10-20 16:11:21 -0700

    gitrsync

--- env/$HOME/jerryc05-bin/gitrsync.py
+++ env/$HOME/jerryc05-bin/gitrsync.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import subprocess as sp
 import sys
 import shutil
@@ -11,36 +12,38 @@ def gitrsync():
     sys.argv.append("--exclude=.git/")
     git = shutil.which("git")
     if git:
-        git_excluded = (
-            sp.check_output([
-                "git",
-                (
-                    "-C"
-                    # [c]urrent directory in which git is called
-                ),
-                sys.argv[1],
-                "ls-files",
-                (
-                    "-z"
-                    # Don't quote and escape utf8; use \0 to separate filenames and keep utf8
-                ),
-                (
-                    "-o"  # [o]ther (not tracked)
-                ),
-                (
-                    "-i"  # [i]gnored
-                ),
-                (
-                    "--exclude-standard"
-                    #  Exclude git-ignored files (respect .gitignore, ...)
-                ),
-                (
-                    "--directory"  # Use directory name if everything is ignored inside
-                ),
-            ])
-            .decode()
-            .split("\0")
-        )
+        git_excluded = []
+        if Path(sys.argv[1]).is_dir():
+            git_excluded = (
+                sp.check_output([
+                    "git",
+                    (
+                        "-C"
+                        # [c]urrent directory in which git is called
+                    ),
+                    sys.argv[1],
+                    "ls-files",
+                    (
+                        "-z"
+                        # Don't quote and escape utf8; use \0 to separate filenames and keep utf8
+                    ),
+                    (
+                        "-o"  # [o]ther (not tracked)
+                    ),
+                    (
+                        "-i"  # [i]gnored
+                    ),
+                    (
+                        "--exclude-standard"
+                        #  Exclude git-ignored files (respect .gitignore, ...)
+                    ),
+                    (
+                        "--directory"  # Use directory name if everything is ignored inside
+                    ),
+                ])
+                .decode()
+                .split("\0")
+            )
         for d in git_excluded:
             d = d.strip()
             if d:
@@ -51,4 +54,7 @@ def gitrsync():
 
 
 if __name__ == "__main__":
+    if sp.check_output(["git", "check-ignore", sys.argv[1]]).strip().__len__():
+        print(f"Path is ignored by git! Quitting ...\n\t{sys.argv[1]}")
+        exit()
     gitrsync()
